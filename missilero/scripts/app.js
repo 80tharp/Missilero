4d5330e54d5671612145c8d6f9001a110ae88e44
80tharp <57098809+80tharp@users.noreply.github.com>
2022-06-08 17:52:27 -0400

    Add files via upload

---
+++ missilero/scripts/app.js
@@ -0,0 +1,286 @@
+var missileArray= [
+    {
+        "missile_cat": 6,
+        "missile_country": "russia",
+        "missile_descript": "1",
+        "missile_id": 1,
+        "missile_name": "SS-21",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "russia",
+        "missile_descript": "2",
+        "missile_id": 2,
+        "missile_name": "SS-22",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "russia",
+        "missile_descript": "3",
+        "missile_id": 3,
+        "missile_name": "SS-23",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "russia",
+        "missile_descript": "4",
+        "missile_id": 4,
+        "missile_name": "SS-24",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "russia",
+        "missile_descript": "5",
+        "missile_id": 5,
+        "missile_name": "SS-25",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "russia",
+        "missile_descript": "6",
+        "missile_id": 6,
+        "missile_name": "SS-26",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "russia",
+        "missile_descript": "7",
+        "missile_id": 7,
+        "missile_name": "SS-27",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "russia",
+        "missile_descript": "8",
+        "missile_id": 8,
+        "missile_name": "SS-28",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "russia",
+        "missile_descript": "9",
+        "missile_id": 9,
+        "missile_name": "SS-29",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "russia",
+        "missile_descript": "0",
+        "missile_id": 10,
+        "missile_name": "SS-30",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "russia",
+        "missile_descript": "11",
+        "missile_id": 11,
+        "missile_name": "SS-31",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "russia",
+        "missile_descript": "12",
+        "missile_id": 12,
+        "missile_name": "SS-32",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "russia",
+        "missile_descript": "22",
+        "missile_id": 13,
+        "missile_name": "SS-33",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "russia",
+        "missile_descript": "33",
+        "missile_id": 14,
+        "missile_name": "SS-34",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "russia",
+        "missile_descript": "44",
+        "missile_id": 15,
+        "missile_name": "SS-35",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "China",
+        "missile_descript": "55",
+        "missile_id": 16,
+        "missile_name": "KN-2",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "China",
+        "missile_descript": "66",
+        "missile_id": 17,
+        "missile_name": "KN-3",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "China",
+        "missile_descript": "77",
+        "missile_id": 18,
+        "missile_name": "KN-4",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "China",
+        "missile_descript": "88",
+        "missile_id": 19,
+        "missile_name": "KN-5",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "China",
+        "missile_descript": "99",
+        "missile_id": 20,
+        "missile_name": "KN-6",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "China",
+        "missile_descript": "00",
+        "missile_id": 21,
+        "missile_name": "KN-7",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "United States",
+        "missile_descript": "111",
+        "missile_id": 22,
+        "missile_name": "patriot1",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "United States",
+        "missile_descript": "222",
+        "missile_id": 23,
+        "missile_name": "patriot2",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "United States",
+        "missile_descript": "333",
+        "missile_id": 24,
+        "missile_name": "patriot3",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "United States",
+        "missile_descript": "444",
+        "missile_id": 25,
+        "missile_name": "patriot4",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "United States",
+        "missile_descript": "555",
+        "missile_id": 26,
+        "missile_name": "patriot5",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "United States",
+        "missile_descript": "666",
+        "missile_id": 27,
+        "missile_name": "patriot6",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 1,
+        "missile_country": "United States",
+        "missile_descript": "777",
+        "missile_id": 28,
+        "missile_name": "patriot7",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 2,
+        "missile_country": "United States",
+        "missile_descript": "888",
+        "missile_id": 29,
+        "missile_name": "patriot8",
+        "missile_range": "CRBM"
+    },
+    {
+        "missile_cat": 3,
+        "missile_country": "United States",
+        "missile_descript": "999",
+        "missile_id": 30,
+        "missile_name": "patriot9",
+        "missile_range": "CRBM"
+    },
+]
+
+
+
+$('th').on('click', function(){
+    var column = $(this).data('column')
+    var order = $(this).data('order')
+    var text = $(this).html()
+    text = text.substring(0, text.length - 1)
+
+    if(order == 'desc'){
+        $(this).data('order', "asc")
+        missileArray2 = missileArray.sort((a,b) => a[column] > b[column] ? 1 : -1)
+        text += '&#9660'
+        console.log(missileArray)
+    }else{
+        $(this).data('order', "desc")
+        missileArray = missileArray.sort((a,b) => a[column] < b[column] ? 1 : -1)
+        text += '&#9650'
+
+    }
+    $(this).html(text)
+    buildTable(missileArray)
+})
+
+
+
+buildTable(missileArray)
+
+function buildTable(data){
+    var table = document.getElementById('missile-data')
+    table.innerHTML = ''
+    for (var i = 0; i < data.length; i++){
+        var row = `<tr>
+                          <td>${data[i].missile_name}</td>
+                          <td>${data[i].missile_cat}</td>
+                          <td>${data[i].missile_range}</td>
+                          <td>${data[i].missile_country}</td>
+                    </tr>`
+        table.innerHTML += row
+
+
+    }
+}
+
